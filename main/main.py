f29a47e580cc8215d9056433f7bae171c98194a9
Ruslan <ruslanpidhainyi02@gmail.com>
2025-11-08 09:47:17 +0100

    add class datetime

--- main/main.py
+++ main/main.py
@@ -1,3 +1,11 @@
-class main:
+from service import datetime
+
+class main(datetime):
     def __init__(self):
         pass
+        
+    def display_current_datetime(self):
+        dt_service = datetime.DateTimeService()
+        current_dt = dt_service.get_current_datetime()
+        print(f"Current Date and Time: {current_dt}")
+
